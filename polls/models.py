8a4728e2915198c1cc62afbaca571900324b08ec
CECE <gaetan.cece@cgi.com>
2023-09-29 20:48:23 +0200

    adding filter sidebar

--- polls/models.py
+++ polls/models.py
@@ -16,7 +16,16 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-#Classe Choice
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
